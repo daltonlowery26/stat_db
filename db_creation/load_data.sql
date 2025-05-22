4524034916a75fa6a91d6ed734a723bb49142886
Dalton Lowery 26 <daltonlowery1@outlook.com>
2025-05-21 22:37:12 -0500

    updating data sources as well as refactoring pulls

--- db_creation/load_data.sql
+++ db_creation/load_data.sql
@@ -1,7 +1,7 @@
 -- Active: 1747588244668@@127.0.0.1@5432@baseball
 -- SQLBook: Code
 
- CREATE DATABASE Baseball;
+CREATE DATABASE Baseball;
 
 DROP DATABASE chinook;
 
@@ -10,28 +10,27 @@ DROP Table fg_02_24;
 CREATE TABLE fg_02_24 (
 	Season INT,
 	Name VARCHAR(100),
-	Team VARCHAR(50),
 	PA INT,
 	Age INT,
-	BB_PCT DECIMAL(7,4),
-	K_PCT DECIMAL(7,4),
-	BABIP DECIMAL(7,3),
-	wRC_Plus DECIMAL(7,4),
-	BsR DECIMAL(7,4),
-	Off DECIMAL(7,4),
-	Def DECIMAL(7,4),
-	WAR DECIMAL(7,1),
-	O_Swing_PCT DECIMAL(7,4),
-	Z_Swing_PCT DECIMAL(7,4),
-	O_Contact_PCT DECIMAL(7,4),
-	Z_Contact_PCT DECIMAL(7,4),
-	Contact_PCT DECIMAL(7,4),
-	SwStr_PCT DECIMAL(7,4),
-	CSW_PCT DECIMAL(7,4)
+	BB_PCT DECIMAL(5,3),
+	K_PCT DECIMAL(5,3),
+	BABIP DECIMAL(5,3),
+	wOBA DECIMAL(5,3),
+	wRC_Plus DECIMAL(6,1),
+	BsR DECIMAL(5,1),
+	Off DECIMAL(5,1),
+	Def DECIMAL(5,1),
+	WAR DECIMAL(5,1),
+	O_Swing_PCT DECIMAL(5,3),
+	Z_Swing_PCT DECIMAL(5,3),
+	Z_Contact_PCT DECIMAL(5,3),
+	Contact_PCT DECIMAL(5,3),
+	SwStr_PCT DECIMAL(5,3),
+	CSW_PCT DECIMAL(5,3)
 );
 
 COPY fg_02_24
-FROM 'C:/Database/data/data_02_24.csv'
+FROM 'C:/Database/data/db/data_02_24.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
 DROP TABLE pitch_18_24;
@@ -43,48 +42,58 @@ CREATE TABLE pitch_18_24 (
 	Batter VARCHAR(100),
 	Pitcher VARCHAR(100),
 	Description VARCHAR(200),
-	Statcast_Zone DECIMAL(7,4),
-	XWOBA DECIMAL(7, 4),
+	Statcast_Zone DECIMAL(7,3),
+	XWOBA DECIMAL(7, 3),
 	B_Stand VARCHAR(10),
 	P_Throws VARCHAR(10),
 	Type VARCHAR(10),
 	Balls INT,
 	Strikes INT,
-	Plate_Horz DECIMAL(7,4),
-	Plate_Vert DECIMAL(7, 4),
-	SZ_Top DECIMAL(7,4),
-	SZ_Bot DECIMAL(7,4)
+	Plate_Horz DECIMAL(7,3),
+	Plate_Vert DECIMAL(7, 3),
+	SZ_Top DECIMAL(7,3),
+	SZ_Bot DECIMAL(7,3)
 );
 
 COPY pitch_18_24
-FROM 'C:/Database/data/pitch_18_24.csv'
+FROM 'C:/Database/data/db/pitch_18_24.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
+DROP TABLE swing_value_18_24
+
 CREATE TABLE swing_value_18_24 (
-	Batter VARCHAR(100),
+	Name VARCHAR(100),
 	Season INT,
 	Chase_Value DECIMAL(7,4),
 	Overall_Value DECIMAL(7,4)
 );
 
 COPY swing_value_18_24
-FROM 'C:/Database/data/swing_values.csv'
+FROM 'C:/Database/data/db/swing_values.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
 
-DROP TABLE swing_time_24;
+DROP TABLE bat_tracking_23_24;
 
-CREATE TABLE swing_time_24 (
+CREATE TABLE bat_tracking_23_24 (
 	Name VARCHAR(100),
 	Season INT,
-	Team VARCHAR(100),
-	Swings INT,
-	Batspeed DECIMAL(7, 4),
-	Swing_Length DECIMAL(7,4),
-	Swing_Time DECIMAL(7, 4),
-	Acceleration DECIMAL(7, 4)
+	Avg_bat_speed DECIMAL(4,2),
+	Fast_swing_rate DECIMAL(4, 2),
+	Blasts_contact DECIMAL(4,2),
+	Blasts_swing DECIMAL(4, 2),
+	Squared_up_contact DECIMAL(4, 2),
+	Squared_up_swing DECIMAL (4,2),
+	Average_swing_length DECIMAL (4,2),
+	Swords INT,
+	Attack_angle DECIMAL(4,2),
+	Attack_direction DECIMAL(4,2),
+	Ideal_angle DECIMAL(4,2),
+	Vertical_swing_path DECIMAL(4,2),
+	Time_to_contact DECIMAL(6, 5)
+
 );
 
-COPY swing_time_24
-FROM 'C:/Database/data/swingtime.csv'
+COPY bat_tracking_23_24
+FROM 'C:/Database/data/db/bat_tracking.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
--- db_queries/ttc_data.sql
+++ db_queries/ttc_data.sql
@@ -1,19 +1,7 @@
-# pulling all data for time to contact
+
 COPY (
-SELECT 
-    t1.name AS name,
-    t1.*,
-    t2.*,
-    t3.*
-FROM
-    fg_02_24 t1
-INNER JOIN
-    swing_time_24 t2 ON t1.name = t2.name
-INNER JOIN
-    swing_value_18_24 t3 ON t1.name = t3.batter
-WHERE
-    t1.season = 2024 AND
-    t2.season = 2024 AND
-    t3.season = 2024
-) TO 'C:/Database/data/db_queries/ttc_research.csv' 
-WITH (FORMAT CSV, HEADER);
+SELECT *
+FROM fg_02_24 f
+LEFT JOIN bat_tracking_23_24 b ON f.name = b.name AND f.season = b.season
+LEFT JOIN swing_value_18_24 s ON f.name = s.name AND f.season = s.season
+) TO 'C:/Database/data/db_queries/ttc.csv' WITH CSV HEADER;
